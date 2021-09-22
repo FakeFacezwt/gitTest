2ca163b30d0a129697b6cd07c760f513fec38471
Enda <1450716485@qq.com>
2021-09-22 19:50:27 +0800

    second commit

--- src/main/java/cn/zous/test.java
+++ src/main/java/cn/zous/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("git hello");
         System.out.println("change");
+        System.out.println("change again");
     }
 }
